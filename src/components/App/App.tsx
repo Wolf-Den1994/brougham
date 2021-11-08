3edf695319258cd9673de6d0a67fe01414ad7875
Denis <wolf-den@tut.by>
2021-11-08 12:16:28 +0300

    feat: add font Montserrat

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,5 @@
 const App: React.FC = () => {
-  return (
-    <ul>
-      <li>5</li>
-      <li>1</li>
-      <li>3</li>
-      <li>4</li>
-      <li>6</li>
-    </ul>
-  );
+  return <div>start</div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import 'normalize.css';
-import './index.css';
+import './index.scss';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
