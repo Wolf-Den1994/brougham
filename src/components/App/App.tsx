9bc0a20b68eb749fb973688441b0524461aa0382
Denis <wolf-den@tut.by>
2021-11-08 12:03:41 +0300

    feat: add eslint and prettier

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,13 @@
-const App = () => {
-    return (
-        <div>
-            Start
-        </div>
-    );
+const App: React.FC = () => {
+  return (
+    <ul>
+      <li>5</li>
+      <li>1</li>
+      <li>3</li>
+      <li>4</li>
+      <li>6</li>
+    </ul>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import 'normalize.css';
 import './index.css';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
